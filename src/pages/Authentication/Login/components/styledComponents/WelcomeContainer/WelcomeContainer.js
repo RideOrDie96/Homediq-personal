639046092e5ba0a61b3fa7c660d209b7e5639031
Stefan <stefan.matic@atessoft.rs>
2021-06-20 20:27:45 +0200

    Initial Commit

---
+++ src/pages/Authentication/Login/components/styledComponents/WelcomeContainer/WelcomeContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const WelcomeContainer = styled.div`
+  width: 450px;
+  height: 660px;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`;
